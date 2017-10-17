59028d6cd916b83a0c582fd94daa84eb32d86959
Daniel Sarfati <danielrsarfati@gmail.com>
2017-10-17 14:55:00 -0700

    Stuff

--- Scripts/installUnity.sh
+++ Scripts/installUnity.sh
@@ -3,7 +3,7 @@
 # Find the latest linux build links here
 # https://forum.unity.com/threads/unity-on-linux-release-notes-and-known-issues.350256/#post-2556301 
 
-LINUX_URL= http://beta.unity3d.com/download/061bcf22327f/unity-editor_amd64-2017.1.0xf3Linux.deb
+LINUX_URL= "http://beta.unity3d.com/download/061bcf22327f/unity-editor_amd64-2017.1.0xf3Linux.deb"
 
 echo 'Downloading Unity pkg:'
 curl --retry 5 -o Unity.deb $LINUX_URL
