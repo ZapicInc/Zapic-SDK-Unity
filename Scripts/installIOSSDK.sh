4b6bc04b00fe6a4a41ea7209455ca4f0aec08956
Daniel Sarfati <danielrsarfati@gmail.com>
2017-10-17 14:30:36 -0700

    Updated build scripts

---
+++ Scripts/installIOSSDK.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo ""
+echo "===================================="
+echo "     Copying (iOS) dependencies     "
+echo "===================================="
+echo ""
+
+#Location for the iOS framework
+iOS_PLUGIN=./Assets/Plugins/iOS
+iOS_FRAMEWORK=$iOS_PLUGIN/Zapic.framework
+
+#Ensure the dir exists
+mkdir -p $iOS_PLUGIN
+
+#Url to the release
+releaseUrl=https://github.com/ZapicInc/Zapic-SDK-iOS/releases/download/${iOS_SDK}/Zapic.framework.zip
+
+#Download the release to the directory
+wget $releaseUrl -P $iOS_PLUGIN
+
+#Unzip the framework
+unzip ${iOS_FRAMEWORK}.zip -d $iOS_PLUGIN
+
+#Remove the zip file
+rm -r $iOS_FRAMEWORK.zip
