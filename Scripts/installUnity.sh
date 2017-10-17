96288ec26a31bca1ef732d9ba9c740713acc5423
Daniel Sarfati <danielrsarfati@gmail.com>
2017-10-17 09:01:03 -0700

    Update travis to use unity 2017.1

--- Scripts/installUnity.sh
+++ Scripts/installUnity.sh
@@ -3,8 +3,8 @@
 # Download Unity3D installer into the container
 #  The below link will need to change depending on the version, this one is for 5.5.1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
-echo 'Downloading Unity 5.5.1 pkg:'
-curl --retry 5 -o Unity.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorInstaller/Unity-5.5.1f1.pkg
+echo 'Downloading Unity pkg:'
+curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/472613c02cf7/MacEditorInstaller/Unity-2017.1.0f3.pkg?_ga=2.96720671.1259806291.1508255378-633858768.1497412801
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 ## In Unity 5 they split up build platform support into modules which are installed separately
