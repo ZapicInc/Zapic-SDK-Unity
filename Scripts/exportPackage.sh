9a8fc5d5d47e5dfc0184cae09d83254c1a080900
Daniel Sarfati <danielrsarfati@gmail.com>
2017-10-17 23:35:44 -0700

    Try no unity

--- Scripts/exportPackage.sh
+++ Scripts/exportPackage.sh
@@ -9,11 +9,18 @@ echo "===================================="
 echo "      Exporting Unity Package       "
 echo "===================================="
 echo ""
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-        -quit \
-        -nographics \
-        -batchmode \
-        -projectPath $(pwd) \
-        -silent-crashes \
-        -logFile $(pwd)/unity.log \
-        -exportPackage Assets/Zapic Assets/Plugins  Zapic.unitypackage
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#         -quit \
+#         -nographics \
+#         -batchmode \
+#         -projectPath $(pwd) \
+#         -silent-crashes \
+#         -logFile $(pwd)/unity.log \
+#         -exportPackage Assets/Zapic Assets/Plugins  Zapic.unitypackage
+
+wget https://github.com/FatihBAKIR/UnityPacker/releases/download/0.0.1/UnityPacker.exe ;
+wget https://github.com/FatihBAKIR/UnityPacker/releases/download/0.0.1/ICSharpCode.SharpZipLib.dll ;
+chmod +x UnityPacker.exe ;
+./UnityPacker.exe Assets/Zapic Zapic no "Assets/Zapic/" "gitignore,md,exe,dll" ".git" ;
+# mv QuarkDefault.unitypackage ~ ;
+# rm -rf /tmp/Quark.Default ;
