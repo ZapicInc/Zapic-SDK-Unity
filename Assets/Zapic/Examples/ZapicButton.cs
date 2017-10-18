e27648fae56d7afe066c7f4f72aaca9721c7d88c
Daniel Sarfati <danielrsarfati@gmail.com>
2017-10-17 23:56:32 -0700

    Cleaned up travis and unity assets

---
+++ Assets/Zapic/Examples/ZapicButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZapicButton : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+        var button = this.GetComponent<Button>();
+
+        if (button == null)
+        {
+            Debug.LogWarning("ZapicButton must have a Button component");
+            return;
+        }
+
+        button.onClick.AddListener(ShowZapicMenu);
+    }
+
+    private void ShowZapicMenu()
+    {
+        Zapic.Show(ZapicSDK.Views.Main);
+    }
+}
